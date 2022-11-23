a2a221e881cbc84c758f79bc4b7f8866bf4ad409
Marilyn Wren <Marilynwren64@yahoo.com>
2022-11-23 11:22:54 -0500

    commit tree 8af08643f83268cf2d95031b403901b190eb283a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,34 @@
 class ApplicationController < Sinatra::Base
-
+set :default_content_type, 'application/json'
   # add routes
+  get '/bakeries' do
+    # get all the bakeries from the database
+    bakeries = Bakery.all
+    # send them back as a JSON array
+    bakeries.to_json
+  end
+  
+  get '/bakeries/:id' do
+   
+    # look up the game in the database using its ID
+    bakery = Bakery.find(params[:id])
+    # send a JSON-formatted response of the game data
+    bakery.to_json(include: :baked_goods)
+  end
+ 
+  get '/baked_goods/:by_price' do
+    # see the BakedGood class for the  method definition of `.by_price`
+    baked_good = BakedGood.by_price
+    baked_good.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/most_expensive' do
+    # see the BakedGood class for the  method definition of `.by_price`
+    baked_good = BakedGood.by_price.first
+    baked_good.to_json
+  end
+
+
+  
 
 end
